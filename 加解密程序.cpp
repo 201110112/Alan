8c165250ed4166593c6b0c0eaaa8440ce507831b
201110112 <101439866+201110112@users.noreply.github.com>
2023-11-11 06:54:20 -0600

    加解密程序.cpp v1.1
    修复了一些已知问题

--- 加解密程序.cpp
+++ 加解密程序.cpp
@@ -1,16 +1,6 @@
-#include <iostream>
-#include <climits>
-#include <algorithm>
-#include <cmath>
-#include <cstring>
-#include <cctype>
-#include <stdio.h>
-#include <queue>
-#include <set>
-#include <stack>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-void jia(string s){
+void encryption(string s){
 	int j[30]={21,35,33,23,13,24,25,26,18,27,28,29,37,36,19,10,11,14,22,15,17,14,12,32,16,31};
 	int a; 
 	for(int i=0;i<s.size();i++){
@@ -19,20 +9,20 @@ void jia(string s){
 	}
 	cout<<" ";
 }
-void jie(string s){
-	char f[11]={'p','q','w','e','r','t','y','u','i','o'};
-	char se[11]={'0','a','s','d','f','g','h','j','k','l'};
-	char t[11]={'0','z','x','c','v','b','n','m'};
+void decrypt(string s){
+	char first[11]={'p','q','w','e','r','t','y','u','i','o'};
+	char second[11]={'0','a','s','d','f','g','h','j','k','l'};
+	char third[11]={'0','z','x','c','v','b','n','m'};
 	int n,m;
 	for(int i=0;i<s.size();i+=2){
 		n=s[i]-'0';
 		m=s[i+1]-'0';
 		if(n==1){
-			cout<<f[m];
+			cout<<first[m];
 		}else if(n==2){
-			cout<<se[m];
+			cout<<second[m];
 		}else if(n==3){
-			cout<<t[m];
+			cout<<third[m];
 		}
 	} 
 	cout<<" ";
@@ -42,9 +32,9 @@ int main(){
 	while(true){
 		cin>>s;
 		if(isalpha(s[0])){
-			jia(s);
+			decrypt(s);
 		}else{
-			jie(s);
+			decrypt(s);
 		}
 	}
 	return 0;
